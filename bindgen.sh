66b34e43df9e3148e339050be6d4f69525f25860
Martin DeMello <martindemello@gmail.com>
2013-10-08 22:47:16 -0700

    move stddef path to a variable

--- bindgen.sh
+++ bindgen.sh
@@ -1,2 +1,3 @@
 BINDGEN=/home/martin/code/rust-bindgen/bindgen
-$BINDGEN -allow-bitfields -x c++ wxHaskell/wxc/src/include/wxc.h `wx-config-2.9 --cflags` -I/home/martin/opt/rust/src/llvm/tools/clang/lib/Headers --include stddef.h --include stdint.h --include time.h -o native.rs
+STDDEF_PATH=/usr/lib/gcc/i686-pc-linux-gnu/4.8.1/include
+$BINDGEN -allow-bitfields -x c++ wxHaskell/wxc/src/include/wxc.h `wx-config-2.9 --cflags` -I$STDDEF_PATH --include stddef.h --include stdint.h --include time.h -o native.rs
